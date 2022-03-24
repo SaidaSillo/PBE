22b06321611b88d8409b355685d06ef0f78b5d90
SaidaSillo <99822243+SaidaSillo@users.noreply.github.com>
2022-03-24 20:34:30 +0100

    Create puzzle1MILLORA.rb

---
+++ puzzle1MILLORA.rb
@@ -0,0 +1,14 @@
+require 'i2c/drivers/lcd'
+display=I2C::Drivers::LCD::Display.new('/dev/i2c-1',0x27)
+puts"Escriu una frase multilinea"
+
+#llegeix fins que hi ha 2 intros i elimina el ultim intro
+frase=gets("\n\n").chop 	
+
+#converteix l'string en un array 			
+array=frase.split("\n")
+
+#printa en el display
+for i in (0..array.length-1)
+	display.text(array[i], i)
+end
