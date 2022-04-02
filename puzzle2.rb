1334d823678e63a7468a78720854afb5322b2b58
SaidaSillo <99822243+SaidaSillo@users.noreply.github.com>
2022-04-02 13:05:34 +0200

    Update puzzle2.rb

--- puzzle2.rb
+++ puzzle2.rb
@@ -1,36 +1,53 @@
-require_relative "lcd.rb"
 require "gtk3"
 require 'i2c/drivers/lcd'
-display=I2C::Drivers::LCD::Display.new('/dev/i2c-1',0x27)
+
+class GtkLcd
+	def init_window
+		@@window = Gtk::Window.new("Puzzle 2")
+		@@window.set_size_request(305, 120)
+		@@window.set_border_width(10)
+
+		@@grid=Gtk::Grid.new		#incloem la quadricula
+		@@window.add(@@grid)
+	end
 	
-window = Gtk::Window.new("Puzzle 2")
-window.set_size_request(305, 120)
-window.set_border_width(10)
+	def finish_window
+		@@window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
+		@@window.show_all
+		Gtk.main
+	end
 
-grid=Gtk::Grid.new
-window.add(grid)
+	def init_button
+		#Creem el botó
+		button = Gtk::Button.new(:label => "Escriure")
+		
+		#Printem a la LCD
+		display=I2C::Drivers::LCD::Display.new('/dev/i2c-1',0x27)
+		button.signal_connect "clicked" do |_widget|
+			array=@@buf.buffer.text.split("\n")
+			display.clear
+			for i in (0..array.length-1)
+				display.text(array[i], i)
+			end
+		end
+		@@grid.attach(button, 0, 1, 9, 1)
+	end
 
-buf=Gtk::TextView.new
-buf.set_size_request(205,80)
-buf.set_monospace(true)
-buf.set_wrap_mode(1)
-
-button = Gtk::Button.new(:label => "Escriure")
-button.signal_connect "clicked" do |_widget|
-  array=buf.buffer.text.split("\n")
-  
-  #printa en el display
-  display.clear
-  for i in (0..array.length-1)
-	display.text(array[i], i)
-  end
+	def init_textView
+		#Creem el recuadre de text
+		@@buf=Gtk::TextView.new
+		@@buf.set_size_request(205,80)
+		@@buf.set_monospace(true)
+		@@buf.set_wrap_mode(1)
+		@@grid.attach(@@buf,3,0,3,1)
+	end
 end
 
-grid.attach(buf,3,0,3,1)
-grid.attach(button, 0, 1, 9, 1)
-
-window.add (grid)
-window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
-window.show_all
-
-Gtk.main
+if __FILE__==$0
+	gtk=GtkLcd.new
+	
+	gtk.init_window
+	gtk.init_button
+	gtk.init_textView
+	gtk.finish_window
+end
